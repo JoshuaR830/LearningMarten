120a769e42941896722a0ea77af334798db77cd4
Joshua <joshua.richardson@myunidays.com>
2019-08-09 10:12:09 +0100

    Added working setup

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Marten;
+
+namespace PracticingMarten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var store = DocumentStore.For("host=localhost;database=postgres;password=password;username=username");
+
+
+            using (var session = store.LightweightSession())
+            {
+                var user = new UserResource
+                {
+                    Name = "Name" 
+                };
+
+                session.Store(user);
+
+                session.SaveChanges();
+            }
+
+            
+            
+
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UserResource.cs
@@ -0,0 +1,7 @@
+using System;
+public class UserResource
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+}
